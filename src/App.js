135a205bdac31f490271c51952396d334c395cb0
Daniil <marytun2003@gmail.com>
2024-06-16 08:43:32 +0300

    tried to add starting carousel. failed

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import MainPage from "./MainPage/MainPage.js"
+import React from 'react';
+
+function App() {
+  return (
+    <div id="app">
+      <MainPage></MainPage>
+    </div>
+  );
+}
+
+export default App;
